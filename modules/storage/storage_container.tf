e0a9727b78a462ca6b2ce083e02c0ce70803abf5
Noah Spannbauer <noahspannbauer@icloud.com>
2025-03-19 09:48:05 -0500

    adding optional storage containers to storage module

---
+++ modules/storage/storage_container.tf
@@ -0,0 +1,6 @@
+resource "azurerm_storage_container" "storage_container" {
+  count = length(var.storage_containers)
+  name                  = "content"
+  storage_account_id    = azurerm_storage_account.storage_account.id
+  container_access_type = "private"
+}
