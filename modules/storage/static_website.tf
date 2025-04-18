3db19589f7c41293d419945d6a3dc14e1490e4c0
Noah Spannbauer <noahspannbauer@icloud.com>
2025-04-18 15:42:57 -0500

    adding static web to storage account

--- modules/storage/static_website.tf
+++ modules/storage/static_website.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account_static_website" "static_website" {
-  count = length(var.storage_tables)
+  count = length(var.static_websites)
   storage_account_id = azurerm_storage_account.storage_account.id
   error_404_document = var.static_websites[count.index].error_404_document
   index_document     = var.static_websites[count.index].index_document
