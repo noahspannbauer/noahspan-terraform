febee5f366f1b01bbea1005ba988a1efb6b39ff5
Noah Spannbauer <noahspannbauer@icloud.com>
2025-08-19 08:04:12 -0500

    updating modules

--- modules/container_app/dns.tf
+++ modules/container_app/dns.tf
@@ -1,16 +1,18 @@
 data "azurerm_dns_zone" "dns_zone" {
+  count = var.custom_domain_count
   name = var.domain_name
 }
 
 data "azurerm_resource_group" "dns_zone_resource_group" {
+  count = var.custom_domain_count
   name = var.dns_zone_resource_group
 }
 
 resource "azurerm_dns_txt_record" "dns_txt_record" {
   count = var.custom_domain_count
   name = "asuid.${var.app_subdomain_name}"
-  resource_group_name = data.azurerm_resource_group.dns_zone_resource_group.name
-  zone_name = data.azurerm_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.dns_zone_resource_group[0].name
+  zone_name = data.azurerm_dns_zone.dns_zone[0].name
   ttl = 14400
 
   record {
