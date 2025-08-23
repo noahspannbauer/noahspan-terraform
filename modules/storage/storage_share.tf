82acb1c6701e8daad6dc178e22815c5bbd078a92
Noah Spannbauer <noahspannbauer@icloud.com>
2025-08-23 09:29:07 -0500

    updating modules

--- modules/storage/storage_share.tf
+++ modules/storage/storage_share.tf
@@ -1,5 +1,6 @@
-resource "azure_storage_share" "storage_share" {
+resource "azurerm_storage_share" "storage_share" {
   count = length(var.storage_shares)
   name = var.storage_shares[count.index]
+  quota = 50
   storage_account_name = azurerm_storage_account.storage_account.name
 }
