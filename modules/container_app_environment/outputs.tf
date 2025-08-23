fc2b09ce1455c7d1cb27def3b36068ae29cc8177
Noah Spannbauer <noahspannbauer@icloud.com>
2025-08-23 09:44:26 -0500

    updating modules

--- modules/container_app_environment/outputs.tf
+++ modules/container_app_environment/outputs.tf
@@ -1,3 +1,7 @@
 output "id" {
   value = azurerm_container_app_environment.container_app_environment.id
 }
+
+output "name" {
+  value = azurerm_container_app_environment.container_app_environment.name
+}
